16fd9bc7dfd67190e263c53469896181429f7a61
S Varun <varunshanker1718@gmail.com>
2024-02-12 01:06:47 +0530

    extend theme

--- Crypto-dashboard/src/main.jsx
+++ Crypto-dashboard/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/ubuntu";
+import theme from "./theme/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ Crypto-dashboard/src/theme/index.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/ubuntu";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "Ubuntu",
+    body: "Ubuntu",
+  },
+});
+
+export default theme;
